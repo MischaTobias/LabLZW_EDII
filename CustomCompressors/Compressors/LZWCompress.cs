453ae22a44738377ad2f5537618a1520802fa740
Bushinii <jorgeg.baldizon@gmail.com>
2020-10-18 19:47:08 -0600

    Compressor complete needs to be tested

--- CustomCompressors/Compressors/LZWCompress.cs
+++ CustomCompressors/Compressors/LZWCompress.cs
@@ -1,11 +1,87 @@
 ﻿using CustomCompressors.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace CustomCompressors.Compressors
 {
     class LZWCompress : ICompressor
     {
+        #region Variables
+        Dictionary<List<byte>, int> LZWTable = new Dictionary<List<byte>, int>();
+        List<byte> Differentchar = new List<byte>();
+        List<byte> Characters = new List<byte>();
+        int code = 1;
+        #endregion
+        public byte[] Compression(byte[] Text)
+        {
+            //Primer recorrido al arreglo
+            var chara = new List<byte>();
+            foreach (var character in Text)
+            {
+                chara[0] = character;
+                if (!LZWTable.ContainsKey(chara))
+                {
+                    LZWTable.Add(chara, code);
+                    code++;
+                    Differentchar.Add(character);
+                }
+            }
+
+            //Segundo recorrido y asignación de valores
+            Characters = Text.ToList<byte>();
+            List<byte> Subchain;
+            string Code = "";
+            int max = 0;
+            while (Characters.Count == 0)
+            {
+                Subchain = new List<byte>();
+                int i = -1;
+
+                while (!LZWTable.ContainsKey(Subchain))
+                {
+                    i++;
+                    Subchain.Add(Characters.ElementAt<byte>(i));
+                }
+                if (max < LZWTable[Subchain])
+                {
+                    max = LZWTable[Subchain];
+                }
+                Code = Convert.ToString(LZWTable[Subchain], 2);
+                for (int j = 0; j < i + 1; j++)
+                {
+                    Characters.RemoveAt(0);
+                }
+            }
+            if (Code.Length != 0)
+            {
+                while (Code.Length % 8 != 0)
+                {
+                    Code += "0";
+                }
+                Convert.ToByte(Code, 2);
+            }
+
+            //Construcción del arreglo final
+            var Finalcode = new List<byte>();
+            Finalcode.Add(Convert.ToByte(Convert.ToString(max, 2).Length));
+            foreach (var item in Differentchar)
+            {
+                Finalcode.Add(item);
+            }
+            var stringBytes = (from Match m in Regex.Matches(Code, @"\d{8}") select m.Value).ToList();
+            foreach (var byteData in stringBytes)
+            {
+                Finalcode.Add(Convert.ToByte(byteData, 2));
+            }
+            return Finalcode.ToArray();
+        }
+        public byte[] Decompression(byte[] CompressedText)
+        {
+            int maxbitlength = CompressedText[0];
+
+        }
     }
 }
--- StringCompressor/Program.cs
+++ StringCompressor/Program.cs
@@ -1,12 +1,21 @@
 ﻿using System;
-
 namespace StringCompressor
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+            { }
+            try
+            {
+                //Insert:
+                Console.WriteLine("Escribe el string a comprimir");
+                string text = Console.ReadLine();
+
+                Console.WriteLine("Se ha guardado el string con éxito para comprimir");
+                string CompressedText = Huff.CompressText(text);
+                Console.WriteLine("El resultado de la compresión es el siguiente:");
+                Console.WriteLine(CompressedText);
+            }
     }
 }
